78b8d39c1e1f82e45906f8ba6ece53924aedcbb4
denisse cumbal <denissecumbal2015@hotmail.com>
2020-08-30 17:43:06 +0000

    realice un comentario

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@ void lectura();
 
 int main() {
 ingresar ();
-/*
- lectura();
- */
-
   return 0;
 }void ingresar (){
   
@@ -20,7 +16,7 @@ ingresar ();
   double  TG;
   double PG;
   double GA;
-
+//guardamos todos los datos y calculos en el archivo inventarioscpp.
   ofstream inventario;
   inventario.open ("inventario.txt",ios::app);
   inventario<<"\n***NUTRI_LECHE ****\n\n";
@@ -29,12 +25,14 @@ ingresar ();
     
   do{
     
-    
+    //pedimos al usuario que ingrese los litros vendidos 
     cout<<"\tIngrese la cantida de litros:";
     cin>>precio;
   cout<<"\tIngrese el valor por galon :";
   cin>>PG;
     cin.ignore();
+    //calculamos el precio de litro en galones 
+    //pasamos de litros a galones 
   TG=precio /(3.785);
   GA=PG*TG;
   inventario<<"*********************************\n\n";
@@ -51,30 +49,11 @@ ingresar ();
     cout<<setw(5)<<"l"<<setw(10)<<precio<<setw(19)<<"ENTERO\n";
   cout<<setw(5)<<"PG"<<setw(10)<<PG<<setw(19)<<"REAL\n";
   cout<<setw(5)<<"TG"<<setw(10)<<TG<<setw(19)<<"REAL\n";
-  cout<<setw(5)<<"GA"<<setw(10)<<GA<<setw(19)<<"REAL\n";
-    
-     
-    
+  cout<<setw(5)<<"GA"<<setw(10)<<GA<<setw(19)<<"REAL\n"; 
     cout<<"Desea ingresar mas datos del dia de ventas : NUTRI_LECHEs(s/*)";
     cin>>d;
 
     cin.ignore();
   }while (d=='s');
-  /*
-  inventario<<setw(5)<<"l"<<setw(10)<<precio<<setw(19)<<"entero\n";
-  inventario<<setw(5)<<"PG"<<setw(10)<<PG<<setw(19)<<"REAL\n";
-  inventario<<setw(5)<<"TG"<<setw(10)<<TG<<setw(19)<<"REAL\n";
-  inventario<<setw(5)<<"GA"<<setw(10)<<GA<<setw(19)<<"REAL\n";
-  */
-
-
-
-
-
-
-
-  
-
-  
   inventario.close();
 }
